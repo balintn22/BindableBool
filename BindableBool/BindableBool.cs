240eca1e1b40456037f1fb5c83e123efb892f147
balintn <balintn@hotmail.com>
2023-02-04 17:38:19 +0000

    Initial implementation.

---
+++ BindableBool/BindableBool.cs
@@ -0,0 +1,56 @@
+﻿/*
+Copyright (c) 2023, Balint Nagy
+All rights reserved.
+*/
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace YA
+{
+    public class BindableBool : INotifyPropertyChanged
+    {
+        private bool _flag;
+
+        public bool Value
+        {
+            get => _flag;
+            set
+            {
+                if (_flag == value)
+                    return;
+
+                _flag = value;
+                OnPropertyChanged(nameof(Value));
+                OnPropertyChanged(nameof(IsTrue));
+                OnPropertyChanged(nameof(IsFalse));
+            }
+        }
+
+        public bool IsTrue { get => _flag; }
+        public bool IsFalse { get => !_flag; }
+
+        public BindableBool(bool b)
+        {
+            _flag = b;
+        }
+
+        public static implicit operator bool(BindableBool bb) => bb._flag;
+        public static implicit operator BindableBool(bool b) => new BindableBool(b);
+
+
+        #region Implement INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # BindableBool
-A bool representation with IsTrue and IsFalse properties, to help WPF/Xamarin/Maui IsVisible/IsNotVisible binding
+A bool representation with IsTrue and IsFalse properties, to help WPF/Xamarin/Maui IsVisible/IsNotVisible binding.
+
+In WPF/Maui, when displaying alternate UI components in certain cases, you bind their IsVisible properties to a bool.
+It is easy for the one that is in sync with the true state, but quite filldy for the false one.
+
+BindableBool implements IsTrue and IsFalse, so that IsVisible can be bound directly.
+
+**Example**
+In your viewModel:
+
+    public BindableBool HasData { get => _myList?.Any() ?? false; }
+
+In your view:
+
+    <Label IsVisible="{Binding HasData.IsFalse}" Text="No Data" />
+
+ for any component that may or may not show:
+
+    <CollectionView IsVisible="{Binding HasData.IsTrue}" .../>
+
+ ... or
+
+    <Image IsVisible="{Binding HasData.Value}" .../>
+
+**Notes**
+- BindabeBool is implicitly convertible to bool so you can use as or compare with a boolean value.
+  You can also do things like
+    BindableBool bb = true;
+    or
+    public BindableBool HasData { get => _myList?.Any() ?? false; }
+
+- BindableBool implements INotifyPropertyChanged such that you can bind to the Value, IsTrue and IsFalse properties as well.
